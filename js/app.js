347a36d71d628a44f9635128aec737d694dae907
kinwail <kinwail@gmail.com>
2018-06-22 19:38:17 -1000

    completed app.js (not final boss)

--- js/app.js
+++ js/app.js
@@ -6,12 +6,25 @@ Find the div with the id of "name1" and replace the n/a with the following:
 
 Tay-Tay*/
 
+var nameGrab = document.getElementById('name1');
+
+// console.log(nameGrab);
+
+nameGrab.innerHTML = "Tay-Tay";
+
+
 
 /*2. DJ Khaled
 
 Find the div with the id of "position2" and replace the n/a with the following: 
 
 Project Manager*/
+
+var positionGrab = document.getElementById('position2');
+
+// console.log(positionGrab);
+
+positionGrab.innerHTML ='Project Manager';
 
 
 /*3. Piko Taro
@@ -20,18 +33,39 @@ Find the div with the id of "alias3" and replace the n/a with the following:
 
 Concatenation*/
 
+var aliasGrab = document.getElementById('alias3');
 
+aliasGrab.innerHTML = "Concatenation";
 
 
 /*4. Prince
 
 Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
 
+var verse = document.getElementsByClassName('profile');
+var nameGrab = document.getElementsByClassName('name');
+//console.log(verse)
+
+
+for (var i =0; i<nameGrab.length;i++){
+    // console.log(nameGrab[i])
+    // console.log(verse[i])
+
+    if (nameGrab[i].innerHTML === 'Prince'){
+        verse[i].innerHTML = '"Purple Rain Something Something Something"'
+    }
+}
 
 
 /*5. Bruce Lee
 
 Find the div with the class name of "profile" and replace the n/a with a quote from the legend himself.*/
+
+ for (i=0;i<nameGrab.length;i++){
+    if (nameGrab[i].innerHTML === 'Bruce Lee'){
+        verse[i].innerHTML = '"Having no limitation as limitation"'
+    }
+ }
 
 
 
@@ -39,7 +73,15 @@ Find the div with the class name of "profile" and replace the n/a with a quote f
 
 Find the div with the class name of "alias" and replace the n/a with your favorite character that Sammy portrayed.*/
 
+var aliasFind = document.getElementsByClassName('alias')
 
+// console.log(aliasFind[2]);
+
+for (i=0;i<nameGrab.length;i++){
+    if (nameGrab[i].innerHTML === 'Samuel L Jackson'){
+    aliasFind[i].innerHTML = 'Ray Arnold (Jurassic Park)'
+    }
+}
 
 /*7. Peter Griffin
 
@@ -47,6 +89,14 @@ Create a div element and give it an id of "name7". Inside this div element, give
 
 Append this div element to the nameParent div*/
 
+var name7 = document.createElement('div');
+var nameAppend = document.getElementById('nameParent')
+
+name7.innerHTML = 'Peter Griffin';
+name7.style.textAlign ='center'
+//console.log(name7);
+
+nameAppend.appendChild(name7);
 
  
 /*8. Tim Duncan
@@ -55,8 +105,25 @@ Create a div element give it an id of "alias8". Inside this div element, give it
 
 Append this div element to the aliasParent div.*/
 
+var alias8 = document.createElement('div');
+var aliasAppend = document.getElementById('aliasParent');
 
+alias8.innerHTML = 'Old Man Riverwalk';
+alias8.style.textAlign ='center';
+
+//console.log(alias8);
+
+aliasAppend.appendChild(alias8);
 
 //Final Boss
 /*9. Create your own profile.*/
 
+var name9 = document.createElement('div');
+var name9Append = document.getElementsByClassName('block3 col-sm-4');
+
+name9.innerHTML = 'Jasen Kaya';
+name9.style.textAlign ='center';
+
+//console.log(name9Append)
+
+// name9Append.append(name9);
